2a74b0ef2af48ac27169a2392efedec63d6c6cba
Jarrad Goulding <43093574-jarradgoulding1@users.noreply.replit.com>
2025-07-31 02:28:17 +0000

    Added development database via neondb - drizzle.config url checking environments.

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,11 +4,16 @@ if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL, ensure the database is provisioned");
 }
 
+let connectionString =
+  process.env.NODE_ENV === "production"
+    ? process.env.DATABASE_URL
+    : process.env.DEV_DATABASE_URL;
+
 export default defineConfig({
   out: "./migrations",
   schema: "./shared/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL,
+    url: connectionString,
   },
 });
