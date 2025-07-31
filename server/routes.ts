e041f342bdb187edf2247efd7c2f30c07a5f08c6
Jarrad Goulding <43093574-jarradgoulding1@users.noreply.replit.com>
2025-07-31 06:08:09 +0000

    cookie set to false as its disrupting login currently

--- server/routes.ts
+++ server/routes.ts
@@ -88,7 +88,7 @@ export async function registerRoutes(app: Express): Promise<Server> {
     resave: false,
     saveUninitialized: false,
     cookie: {
-      secure: isProd, // Set to true in production with HTTPS
+      secure: false, // Set to true in production with HTTPS
       httpOnly: true,
       maxAge: 24 * 60 * 60 * 1000, // 24 hours
     },
