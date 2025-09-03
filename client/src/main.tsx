65e2584861405576559069daba8caf0191e3e6d8
jarradgoulding1 <43093574-jarradgoulding1@users.noreply.replit.com>
2025-09-03 01:04:15 +0000

    Restored to '0bf63732f6fafcae6d6428c1e806c55e7ae5f0be'
    Replit-Restored-To: 0bf63732f6fafcae6d6428c1e806c55e7ae5f0be

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,6 +2,4 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-console.log("hello world");
-
 createRoot(document.getElementById("root")!).render(<App />);
